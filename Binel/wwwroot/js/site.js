eb08c0bc8ee50da3ba8d33d1beab185543e657db
Serkanjim <74598689+Serkanjim@users.noreply.github.com>
2024-05-20 23:14:45 +0300

    About, login, and register views have been added. New CSS files has been included, and the site.js file has been updated.

--- Binel/wwwroot/js/site.js
+++ Binel/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function isValidEmail(email) {
+    // Email için geçerli bir düzenli ifade
+    var emailRegex = /^[^\s@]+@@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+
+
+
+
+});
+
+
+//Bu kodlar, fare arama butonunun üzerine geldiğinde ve üzerinden çekildiğinde resminin değişmesini sağlar. search-bar-01.png ve search-bar-02.png dosyalarının gerçek yolunu belirterek bu kısmı güncelleyebilirsiniz.
+function changeImage() {
+    document.getElementById("search-img").src = "search-bar-02.png";
+}
+
+function restoreImage() {
+    document.getElementById("search-img").src = "search-bar-01.png";
+}
